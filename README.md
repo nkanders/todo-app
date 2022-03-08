372dfb685c78e7710eb50ca44300e6e212bad3b6
nkanders <nicandersonbusin@gmail.com>
2022-03-07 18:51:08 -0600

    init

---
+++ README.md
@@ -0,0 +1,27 @@
+## Getting started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. 
+
+See deployment for notes on how to deploy the project on a live system.
+### Prerequisites
+To get this project up and running on your local system you'll need both yarn and node.js installed on your development system.
+
+### Installing
+To install all dependencies required for the project, clone or download the source cd into the project root and from your terminal run:
+```bash
+yarn install
+```
+
+### Local development / testing
+The project uses Create React App, which includes a local development server cd into the project root and run the following command from the terminal:
+```bash
+yarn start
+```
+
+Create React App will build a development version of the app. The localhost / local network preview address will be printed to the console.
+
+### Deployment
+To build a production copy for deployment cd into the project root and run the following command from your terminal:
+```bash
+yarn run build
+```
---
+++ src/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import configureStore  from './store/configureStore';
+
+import './index.css';
+import App from './components/App';
+import * as serviceWorker from './serviceWorker';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+
+
+const THEME = createMuiTheme({
+  typography: {
+   "fontFamily": `-apple-system,BlinkMacSystemFont,Segoe UI,Helvetica,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji`,
+  },
+  overrides: {
+    MuiButton: {
+      root: {
+        margin: '5px 0 5px',
+      },
+    },
+  },
+});
+
+const store = configureStore();
+
+ReactDOM.render(
+  <MuiThemeProvider theme={THEME}>
+    <Provider store={store}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Provider>
+  </MuiThemeProvider>,
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
